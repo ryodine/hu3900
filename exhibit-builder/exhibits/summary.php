83130204c77ab748e7c37cc6c4627baa46c62802
Ryan Johnson <ryan@johnsonweb.us>
2019-04-16 15:40:58 -0400

    exhibit builder empty template

--- exhibit-builder/exhibits/summary.php
+++ exhibit-builder/exhibits/summary.php
@@ -1,57 +1,29 @@
 <?php echo head(array('title' => metadata('exhibit', 'title'), 'bodyclass'=>'exhibits summary')); ?>
 
-<?php set_exhibit_pages_for_loop_by_exhibit(); ?>
-<?php foreach (loop('exhibit_page') as $exhibitPage): ?>
-<?php endforeach;?>
+<h1><?php echo metadata('exhibit', 'title'); ?></h1>
+<?php echo exhibit_builder_page_nav(); ?>
 
-<div class="exhibit-internal-wrap">
-<h1 style="width:100%"><?php echo metadata('exhibit', 'title'); ?></h1>
-
-<?php if (get_theme_option('show_on_summary') == 1): ?>
+<div id="primary">
 <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
 <div class="exhibit-description">
     <?php echo $exhibitDescription; ?>
 </div>
 <?php endif; ?>
-<?php foreach (loop('exhibit_page') as $exhibitPage): ?>
-    <h2 class="exhibit_title"><?php echo $exhibitPage->title; ?></h2>
-    <?php exhibit_builder_render_exhibit_page($exhibitPage); ?>
-<?php endforeach; ?>
 <?php if (($exhibitCredits = metadata('exhibit', 'credits'))): ?>
 <div class="exhibit-credits">
     <h3><?php echo __('Credits'); ?></h3>
     <p><?php echo $exhibitCredits; ?></p>
 </div>
 <?php endif; ?>
-<?php endif;?>
-
-<?php if (get_theme_option('show_on_summary') == 0): ?>
-
-    <div class="internal_summary_descriptor">
-    <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
-    <div class="exhibit-description">
-        <?php echo $exhibitDescription; ?>
-    </div>
-    <?php endif; ?>
-
-    <?php if (($exhibitCredits = metadata('exhibit', 'credits'))): ?>
-    <div class="exhibit-credits">
-        <h3><?php echo __('Credits'); ?></h3>
-        <p><?php echo $exhibitCredits; ?></p>
-    </div>
-    <?php endif; ?>
-    </div>
-
-    <?php
-    $pageTree = exhibit_builder_page_tree();
-    if ($pageTree):
-    ?>
-    <nav id="exhibit-pages">
-        <?php echo $pageTree; ?>
-    </nav>
-    <?php endif; ?>
-<?php endif; ?>
-
 </div>
+
+<?php
+$pageTree = exhibit_builder_page_tree();
+if ($pageTree):
+?>
+<nav id="exhibit-pages">
+    <?php echo $pageTree; ?>
+</nav>
+<?php endif; ?>
 
 <?php echo foot(); ?>
