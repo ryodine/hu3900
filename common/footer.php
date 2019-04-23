9b037dc50a28ff1318e342831cd108316be75894
Ryan Johnson <ryan@johnsonweb.us>
2019-04-23 15:19:08 -0400

    #12: All three on right

--- common/footer.php
+++ common/footer.php
@@ -1,13 +1,16 @@
 </div><!-- end content -->
 
 <?php
-    function footer_logo_uri() {
-        $footer_logo = get_theme_option('footer_logo');
+    function footer_logo_uri($n="") {
+        $footer_logo = get_theme_option('footer_logo'. $n);
         if ($footer_logo) {
             $storage = Zend_Registry::get('storage');
             $uri = $storage->getUri($storage->getPathByType($footer_logo, 'theme_uploads'));
             return $uri;
         }
+    }
+    function footer_link_url($n="") {
+        return ((get_theme_option('footer_url'. $n) == null) ? '#' : get_theme_option('footer_url' . $n));
     }
 ?>
 
@@ -23,9 +26,23 @@
         <p><?php echo $copyright; ?></p>
         <?php endif; ?>
         <nav><?php echo public_nav_main()->setMaxDepth(0); ?></nav>
+        <div class="flogos">
         <?php if (get_theme_option('footer_logo') != null): ?>
-            <a class="wpilogo" href="https://wpi.edu" target="_blank" style="background-image:url('<?php echo footer_logo_uri(); ?>');"></a>
+            <a class="footlogo" href="<?php echo footer_link_url(); ?>" target="_blank" style="background-image:url('<?php echo footer_logo_uri(); ?>');">
+                <img src="<?php echo footer_logo_uri(); ?>" />
+            </a>
         <?php endif; ?>
+        <?php if (get_theme_option('footer_logo2') != null): ?>
+            <a class="footlogo" href="<?php echo footer_link_url("2"); ?>" target="_blank" style="background-image:url('<?php echo footer_logo_uri("2"); ?>');">
+                <img src="<?php echo footer_logo_uri("2"); ?>" />
+            </a>
+        <?php endif; ?>
+        <?php if (get_theme_option('footer_logo3') != null): ?>
+            <a class="footlogo" href="<?php echo footer_link_url("3"); ?>" target="_blank" style="background-image:url('<?php echo footer_logo_uri("3"); ?>');"><img src="
+                <?php echo footer_logo_uri("3"); ?>" />
+            </a>
+        <?php endif; ?>
+        </div>
     </div><!-- end footer-content -->
 
      <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
